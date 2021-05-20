67c19f30dc4a7a1a2dd1d58082b80ccd8aadfe75
Rishab Kurapati <63502591+itsmerishab@users.noreply.github.com>
2021-05-20 20:33:43 +0530

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,10 +1,4 @@
-const cur = document.querySelector('#cursor');
+const cursor = document.querySelector('#cursor');
 document.addEventListener('mousemove' , e => {
-          cur.setAttribute("style", "top:"+(e.pageY -15) + "px; left:"+(e.pageX-17)+"px; transition: .1s")
-          console.log(e);
+          cursor.setAttribute("style", "top:"+(e.pageY -15) + "px; left:"+(e.pageX-17)+"px;")
 })
-
-// const con = document.querySelector('.crsr');
-// document.addEventListener('', q=>{
-//      cur.setAttribute("style", "visibility: none")
-// })
